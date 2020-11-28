eda9be0b85073fbfd2367398a02f25566407dfdf
Karina Brezgina <brezgina.karinka@yandex.ru>
2020-11-28 15:01:24 +0300

    7

--- javascript/index.js
+++ javascript/index.js
@@ -4,32 +4,31 @@ let profileCloseButtonNode = document.querySelector('.popup__close-button')
 let formElement = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__name')
 let profileJob = document.querySelector('.profile__job')
-let nameInput = document.querySelector('.popup__field_name');
-let jobInput = document.querySelector('.popup__field_job'); 
+let nameInput = document.querySelector('.popup__field_name_value');
+let jobInput = document.querySelector('.popup__field_job_value'); 
 
-profileButtonNode.addEventListener ('click', hadlyPopupOpenButton)
+function getProfileInfo(){
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+};
 
 function hadlyPopupOpenButton() {
     popupNode.classList.add('popup_opened');
     getProfileInfo()
-}
-;
+};
 
 function handlePopupCloseButton() {
     popupNode.classList.remove('popup_opened');
 
 };
 
-function getProfileInfo(){
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-}
-
 function formSubmitHandler (event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     handlePopupCloseButton();
-}
+};
+
+profileButtonNode.addEventListener ('click', hadlyPopupOpenButton)
 profileCloseButtonNode.addEventListener('click', handlePopupCloseButton);
 formElement.addEventListener('submit', formSubmitHandler);
