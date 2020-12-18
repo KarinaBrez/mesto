04a8b3ffbafb7cfb226af8340d49e2c094b9c6bc
Karina Brezgina <brezgina.karinka@yandex.ru>
2020-12-18 17:18:09 +0300

    13

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
-let profileButtonNode = document.querySelector('.profile__button-edit')
+const profileButtonNode = document.querySelector('.profile__button-edit')
 const popupNode = document.querySelector('.popup')
-let profileCloseButtonNode = document.querySelector('.popup__close-button')
+const profileCloseButtonNode = document.querySelector('.popup__close-button')
 let formElement = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__name')
 let profileJob = document.querySelector('.profile__job')
@@ -11,7 +11,7 @@ let popupEdForm = document.querySelector('#popup_ed')
 let popupAddImage = document.querySelector('#popup_add')
 let popupImageBig = document.querySelector('#popup_img')
 let imageAddButton = document.querySelector('.profile__button-add')
-let popImageAddCloseButton = document.querySelector('.popup__close-button-add')
+const popImageAddCloseButton = document.querySelector('.popup__close-button-add')
 let placeInput = document.querySelector('.popup__field_value_place')
 let linkInput = document.querySelector('.popup__field_value_link')
 let fornAdd = document.querySelector('.popup__form_add')
@@ -47,8 +47,6 @@ const initialCards = [
 
 function hadlyPopupOpenButton(popupNode) {
     popupNode.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
 };
 
 function handlePopupCloseButton(popupNode) {
@@ -64,7 +62,11 @@ function formSubmitHandler (event) {
 };
 
 //Открытие и закрытие поп-апа с формой
-profileButtonNode.addEventListener ('click',()=> hadlyPopupOpenButton(popupEdForm))
+profileButtonNode.addEventListener ('click',()=> hadlyPopupOpenButton(popupEdForm),()=>{
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;  
+}
+)
 profileCloseButtonNode.addEventListener('click', ()=> handlePopupCloseButton (popupEdForm));
 formElement.addEventListener('submit', formSubmitHandler);
 
@@ -85,9 +87,11 @@ function readerList (){
 function composeItem(item) {
     const templateElement = document.querySelector('.template').content;
     const newItem = templateElement.cloneNode(true);
-    newItem.querySelector('.element__image').src = item.link;
-    newItem.querySelector('.element__title').textContent = item.name;
-    newItem.querySelector('.element__image').textContent = item.name;
+    const imageCard = newItem.querySelector('.element__image');
+    const titleCard = newItem.querySelector('.element__title').textContent = item.name;
+    imageCard.src = item.link;
+    titleCard.textContent = item.name;
+    imageCard.alt = item.name;
     newItem.querySelector('.element__button').addEventListener('click', function (evt) {
         evt.target.classList.toggle('element__button_active');
     });
@@ -96,12 +100,14 @@ function composeItem(item) {
     })
     newItem.querySelector('.element__image').addEventListener('click', (event) => {
             hadlyPopupOpenButton(popupImageBig)
-            const  popupCard = event.target.closest('.element')
-            let popupPicture = popupCard.querySelector('.element__image')
+            const popupCard = event.target.closest('.element')
+            const popupPicture = popupCard.querySelector('.element__image')
             let popupDescription = popupCard.querySelector('.element__title')
-            document.querySelector('.popup__image').src = popupPicture.src
-            document.querySelector('.popup__title').textContent = popupDescription.textContent
-            document.querySelector('.popup__image').textContent = popupDescription.textContent
+            const imagePopup = document.querySelector('.popup__image')
+            const titlePopup = document.querySelector('.popup__title')
+            imagePopup.src = popupPicture.src
+            titlePopup.textContent = popupDescription.textContent
+            imagePopup.alt = popupDescription.textContent
         })
         
     return newItem;  
